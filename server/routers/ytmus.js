b43823514979b985f47d11a088c944bae8da98ce
PRTYBYZ <68802750+partyboyz@users.noreply.github.com>
2021-01-14 03:06:16 +0700

    Update ytmus.js

--- server/routers/ytmus.js
+++ server/routers/ytmus.js
@@ -21,7 +21,7 @@ async function ytMusic(URL) {
     let getVideo = await page.$eval('#download > div > div > a', (element) => {
         return element.getAttribute('href');
     });
- let titleInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(1)', el => el.innerText);
+ let titleInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:b:nth-child(1)', el => el.innerText);
  let sizeInfo = await page.$eval('body > div:nth-child(1) > div:nth-child(4) > div > div.col-md-9 > p:nth-child(2)', el => el.innerText); 
 	browser.close();
     return {titleInfo, getImages, getVideo, sizeInfo}
