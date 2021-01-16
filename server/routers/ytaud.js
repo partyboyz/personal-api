4df894fe7ad1b682572e70131ec179ed046e7b13
PRTYBYZ <68802750+partyboyz@users.noreply.github.com>
2021-01-16 17:16:17 +0700

    Update ytaud.js

--- server/routers/ytaud.js
+++ server/routers/ytaud.js
@@ -8,15 +8,16 @@ async function ytAudio(URL) {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('https://ytmp3.cc/en13/',{delay: 300} )
+    await page.goto('https://ytmp3.cc/en13/');
         
-    await page.type('#input', `${URL}`, {delay: 300})
+    await page.type('#input', `${URL}`, {delay: 300});
+    await page.waitForSelector('#submit');
     await page.click('#submit');
     await page.waitForSelector('#submit');
     let getVideo = await page.$eval('body > div:nth-child(2) > div > div:nth-child(2) > a:nth-child(1)', (element) => {
         return element.getAttribute('href');
     });
-    let titleInfo = await page.$eval('body > div:nth-child(2) > div > div:nth-child(1)', el => el.innerText);
+    let titleInfo = await page.$eval('#title', el => el.innerText);
     browser.close();
      return {
 	    titleInfo,
