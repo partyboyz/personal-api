0d51966b0a910e99080f3cb7c5246905af098cb2
PRTYBYZ <68802750+partyboyz@users.noreply.github.com>
2021-01-17 13:03:37 +0700

    Create ytmp3.js

---
+++ server/routers/ytmp3.js
@@ -0,0 +1,34 @@
+const ytmp3 = require('express'):
+const puppeteer = require('puppeteer');
+
+async function ytMp3(URL) {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://youtube-to-mp3.org/'); 
+
+    await page.type('#input', `${URL}`);
+    await page.click('#submit', {delay: 300});
+    await page.waitForSelector('#response > div > div.y_thumb > img');
+    let Img = await page.$eval('#response > div > div.y_thumb > img', (image) => {
+        return image.getAttribute('src');
+    });
+    await page.waitForSelector('#tab_mp3 > tbody > tr:nth-child(1) > td:nth-child(3) > a');
+    let Link = await page.$eval('#tab_mp3 > tbody > tr:nth-child(1) > td:nth-child(3) > a', (element) => {
+        return element.getAttribute('href');
+    });
+    let Title = await page.$eval('#response > div > div.y_thumb > div.title', el => el.innerText);
+    let FileS = await page.$eval('#tab_mp3 > tbody > tr:nth-child(1) > td:nth-child(2)', size => size.innerText);
+    //let Upload = await page.$eval('#search-result > div > div > div > div > p:nth-child(2)', size => size.innerText);
+    browser.close();
+     return {Title,Link,Img,FileS}
+    }
+    ytmp3.get('/', async (req, res) => {
+        var URL = req.query.URL;
+        const gets = await ytMp3(URL);
+        res.json(gets);
+        
+    });
+    module.exports = ytmp3;
